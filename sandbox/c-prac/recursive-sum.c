2851a3daac6270ad9dee0ee291c16520be7174f9
otukutun <orehaorz@gmail.com>
2021-06-09 09:56:21 +0900

    Add recursive sum sample program.

---
+++ sandbox/c-prac/recursive-sum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int recursive_sum(int v)
+{
+  printf("calling with %d\n", v);
+
+  if (v == 0) {
+      return 0;
+  }
+
+  return v + recursive_sum(v - 1);
+}
+
+int main(void)
+{
+  int max, v;
+  printf("Please input maxium number\n");
+  scanf("%d", &max);
+
+  v = recursive_sum(max);
+
+  printf("Sum is %d\n", v);
+}
+
